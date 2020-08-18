48803311d570797088c3fc21bd85b73bb698df27
zack-murray <61975667+zack-murray@users.noreply.github.com>
2020-08-18 19:07:42 -0400

    Tuesday assignment, mvp complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,6 +6,9 @@ import sys
 HLT = 0b00000001 # exits emulator
 LDI = 0b10000010 # sets a specified register to a value
 PRN = 0b01000111 # print value stored in register
+ADD = 0b10100000 # add value of two registers & store result in registerA
+SUB = 0b10100001 # subtract the value in the second register from the first & store result in registerA
+MUL = 0b10100010 # multiply the values in two registers & store result in registerA
 
 class CPU:
     """Main CPU class."""
@@ -29,34 +32,55 @@ class CPU:
         """Accept a value to write, and the address to write it to"""
         self.ram[address] = value
 
-    def load(self):
+    def load(self, filename=None):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        # If not being fed 2 programs 
+        if len(sys.argv) != 2:
+            print("usage: comp.py progname")
+            sys.exit(1)
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        try:
+            with open(filename, 'r') as f:
+                for line in f:
+                    # Split line at # character, use elements be4 split
+                    line = line.split("#")[0].strip()
+                    if len(line) == 0:
+                        continue
+                    # If first character starts with #, ignore it
+                    if line[0][0] == '#':
+                        continue
+                    # Since we're working in binary, have to set base to 2
+                    try:
+                        self.ram[address] = int(line, 2)
+                    # Raise error if not fed appropriate int
+                    except ValueError:
+                        print(f'Invalid number: {line}')
+                        sys.exit(1)
+                
+                    address += 1
+        
+        # Not given a valid filename
+        except FileNotFoundError:
+            print(f"Couldn't open {sys.argv[1]}")
+            sys.exit(2)
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+        # No instructions given
+        if address == 0:
+            print("Program was empty!")
+            sys.exit(3)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op =='SUB':
+            self.reg[reg_a] -= self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -82,6 +106,7 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
+
         while self.running:
             # Instantiate the instruction register
             ir = self.ram_read(self.pc)
@@ -96,8 +121,24 @@ class CPU:
                 self.reg[operand_a] = operand_b
                 # Increment the counter
                 self.pc += 3
-                # Print numeric value stored in the given register
+            # Print numeric value stored in the given register
             elif ir == PRN:
                 print(self.reg[operand_a])
                 # Increment the counter
                 self.pc += 2
+            # Add value of two registers & store result in registerA    
+            elif ir == ADD:
+                self.alu("ADD", operand_a, operand_b)
+                # Increment the counter
+                self.pc += 3
+            # Subtract the value in the second register from the first & 
+            # store result in registerA
+            elif ir == SUB:
+                self.alu("SUB", operand_a, operand_b)
+                # Increment the counter
+                self.pc += 3
+            # Multiply the values in two registers & store result in registerA
+            elif ir == MUL:
+                self.alu("MUL", operand_a, operand_b)
+                # Increment the counter
+                self.pc += 3
